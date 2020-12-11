c089134d11b36d4bdfa49c50901af2fdd5572a3d
basilmaben <70912563+basilmaben@users.noreply.github.com>
2020-12-11 23:57:48 +0530

    more Styling practice

--- src/index.js
+++ src/index.js
@@ -1,15 +1,29 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+const date = new Date();
+const currentTime = date.getHours();
+
+let greeting;
+
 const customStyle = {
-  color: "red",
-  fontSize: "20px",
-  border: "1px solid black"
+  color: ""
 };
 
-customStyle.color = "blue";
+if (currentTime < 12) {
+  greeting = "Good Morning";
+  customStyle.color = "red";
+} else if (currentTime < 18) {
+  greeting = "Good Afternoon";
+  customStyle.color = "green";
+} else {
+  greeting = "Good Night";
+  customStyle.color = "blue";
+}
 
 ReactDOM.render(
-  <h1 style={customStyle}>Hello World!</h1>,
+  <h1 className="heading" style={customStyle}>
+    {greeting}
+  </h1>,
   document.getElementById("root")
 );
